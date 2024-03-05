a71744a33002de78f6d5417b336508f132b6ff47
marco <longhkt10@gmail.com>
2024-03-05 11:44:00 +0700

    Add Auto Testing From Excel File

--- Test2/Demo.cs
+++ Test2/Demo.cs
@@ -17,7 +17,7 @@ namespace Test2
     internal class Demo
     {
         IWebDriver driver;
-        public static IEnumerable<TestCaseData> GetTestCaseDatasFromExcel()
+        public static IEnumerable<TestCaseData> GetTestCaseDatasFromExcel(string sheetName)
         {
             var testData = new List<TestCaseData>();
             using (var stream = File.Open("TestCaseData.xlsx", FileMode.Open, FileAccess.Read))
@@ -25,7 +25,7 @@ namespace Test2
                 using (var reader = ExcelReaderFactory.CreateReader(stream))
                 {
                     var result = reader.AsDataSet();
-                    var table = result.Tables[0];
+                    var table = result.Tables[sheetName];
                     for (int i= 0; i < table.Rows.Count; i++)
                     {
                         double num1 = Convert.ToDouble(table.Rows[i][0].ToString());
@@ -55,15 +55,25 @@ namespace Test2
         }
 
         [Test]
-        public void TestCalcAdd()
+        [TestCaseSource(nameof(GetTestCaseDatasFromExcel), new object[] { "Sub" })]
+        public void TestCalcAdd(double a, double b, double expected)
         {
+            string sheetname = "Add";
             Test();
-            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys("11");
-            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys("11");
+            string num1 = a.ToString();
+            string num2 = b.ToString();
+            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys(num1);
+            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys(num2);
             driver.FindElement(By.XPath("//input[@id='calculateButton']")).Click();
             Thread.Sleep(1000);
-            string actual = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
-            string expected = (11 + 11).ToString();
+            string result = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
+            double actual = Convert.ToDouble(result);
+            //if (actual != Convert.ToString(expected))
+            //{
+            //    Console.WriteLine(actual);
+            //    Console.WriteLine(expected);
+            //    Console.WriteLine("False");
+            //}
             if (actual != expected)
             {
                 Console.WriteLine(actual);
@@ -76,18 +86,23 @@ namespace Test2
                 Console.WriteLine(expected);
                 Console.WriteLine("True");
             }
+            WriteDataToExcel(Convert.ToString(actual), sheetname);
         }
         [Test]
-        public void TestCalcSub()
+        [TestCaseSource(nameof(GetTestCaseDatasFromExcel), new object[] { "Sub" })]
+        public void TestCalcSub(double a, double b, double expected)
         {
+            string sheetname = "Sub";
             Test();
-            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys("16");
-            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys("11");
+            string num1 = a.ToString();
+            string num2 = b.ToString();
+            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys(num1);
+            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys(num2);
             driver.FindElement(By.XPath("//option[normalize-space()='Subtract']")).Click();
             driver.FindElement(By.XPath("//input[@id='calculateButton']")).Click();
             Thread.Sleep(1000);
-            string actual = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
-            string expected = (16 - 11).ToString();
+            string result = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
+            double actual = Convert.ToDouble(result);
             if (actual != expected)
             {
                 Console.WriteLine(actual);
@@ -100,18 +115,23 @@ namespace Test2
                 Console.WriteLine(expected);
                 Console.WriteLine("True");
             }
+            WriteDataToExcel(Convert.ToString(actual), sheetname);
         }
         [Test]
-        public void TestCalcMulti()
+        [TestCaseSource(nameof(GetTestCaseDatasFromExcel), new object[] { "Multi" })]
+        public void TestCalcMulti(double a, double b, double expected)
         {
+            string sheetname = "Multi";
             Test();
-            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys("16");
-            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys("11");
+            string num1 = a.ToString();
+            string num2 = b.ToString();
+            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys(num1);
+            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys(num2);
             driver.FindElement(By.XPath("//option[normalize-space()='Multiply']")).Click();
             driver.FindElement(By.XPath("//input[@id='calculateButton']")).Click();
             Thread.Sleep(1000);
-            string actual = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
-            string expected = (16 * 11).ToString();
+            string result = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
+            double actual = Convert.ToDouble(result);
             if (actual != expected)
             {
                 Console.WriteLine(actual);
@@ -124,18 +144,23 @@ namespace Test2
                 Console.WriteLine(expected);
                 Console.WriteLine("True");
             }
+            WriteDataToExcel(Convert.ToString(actual), sheetname);
         }
         [Test]
-        public void TestCalcDivi()
+        [TestCaseSource(nameof(GetTestCaseDatasFromExcel), new object[] { "Divi" })]
+        public void TestCalcDivi(double a, double b, double expected)
         {
+            string sheetname = "Divi";
             Test();
-            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys("16");
-            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys("11");
+            string num1 = a.ToString();
+            string num2 = b.ToString();
+            driver.FindElement(By.XPath("//input[@id='number1Field']")).SendKeys(num1);
+            driver.FindElement(By.XPath("//input[@id='number2Field']")).SendKeys(num2);
             driver.FindElement(By.XPath("//option[normalize-space()='Divide']")).Click();
             driver.FindElement(By.XPath("//input[@id='calculateButton']")).Click();
             Thread.Sleep(1000);
-            string actual = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
-            string expected = (16 / 11).ToString();
+            string result = driver.FindElement(By.XPath("//input[@id='numberAnswerField']")).GetAttribute("value");
+            double actual = Convert.ToDouble(result);
             if (actual != expected)
             {
                 Console.WriteLine(actual);
@@ -148,12 +173,14 @@ namespace Test2
                 Console.WriteLine(expected);
                 Console.WriteLine("True");
             }
+            WriteDataToExcel(Convert.ToString(actual), sheetname);
         }
         [Test]
-        [TestCaseSource("GetTestCaseDatasFromExcel")]
-                
+        [TestCaseSource(nameof(GetTestCaseDatasFromExcel), new object[] { "Conca" })]
+
         public void TestCalcConca(double a, double b, double expected)
         {
+            string sheetname = "Conca";
             Test();
             string num1 = a.ToString();
             string num2 = b.ToString();
@@ -176,7 +203,7 @@ namespace Test2
                 Console.WriteLine(expected);
                 Console.WriteLine("True");
             }
-            WriteDataToExcel(Convert.ToString(actual));
+            WriteDataToExcel(Convert.ToString(actual), sheetname);
         }
         [TearDown]
         public void TearDown()
@@ -184,7 +211,7 @@ namespace Test2
             
             driver.Quit();
         }
-        public void WriteDataToExcel(String Actual)
+        public void WriteDataToExcel(String Actual, String SheetName)
         {
             // Đường dẫn của tệp Excel đích
             string excelFilePath = "TestCaseData.xlsx";
@@ -192,8 +219,8 @@ namespace Test2
             // Tạo một tệp Excel mới
             using (var excelPackage = new ExcelPackage(new FileInfo(excelFilePath)))
             {
-                // Lấy hoặc tạo một Sheet có tên "Result"
-                var worksheet = excelPackage.Workbook.Worksheets["Sheet1"];
+                // Lấy hoặc tạo một Sheet có tên được truyền vào
+                var worksheet = excelPackage.Workbook.Worksheets[SheetName];
 
                 // Ghi dữ liệu vào các ô trong Sheet
                 int lastRow = 1;
